b98e650102a19e30c49da2d06c8b838c643bafc6
LaBay <labay@i.ua>
2016-03-11 14:58:00 +0200

    files added

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+var a;
+var n;
+a = prompt('Введите число:');
+n = prompt('Введите степень числа:');
+
+while (n < 0) {
+	n = prompt('Введите положительное значение степени:');
+}
+
+function res(a){
+	var result = a;
+	for (var i = 1; i < n; i++) {
+		result = result*a;
+	}
+	alert('Число ' + a + ' в ' + n + '-й степени равно ' + result);
+}
+res(a);
